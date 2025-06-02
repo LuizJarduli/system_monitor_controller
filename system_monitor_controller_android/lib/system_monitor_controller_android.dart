f4caeb7802254722fb66452003c59148b7448e66
Luiz Jarduli <luizjarduli@gmail.com>
2025-06-01 22:52:19 -0300

    feat: add initial files and structure for system_monitor_controller_android plugin

---
+++ system_monitor_controller_android/lib/system_monitor_controller_android.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:system_monitor_controller_platform_interface/system_monitor_controller_method_channel.dart';
+import 'package:system_monitor_controller_platform_interface/system_monitor_controller_platform_interface.dart';
+
+class SystemMonitorControllerAndroid extends SystemMonitorControllerPlatform {
+  SystemMonitorControllerAndroid({
+    @visibleForTesting MethodChannelSystemMonitorController? channelApi,
+  }) : _channelApi = channelApi ?? MethodChannelSystemMonitorController();
+
+  final MethodChannelSystemMonitorController _channelApi;
+
+  @override
+  Future<String?> getPlatformVersion() {
+    return _channelApi.getPlatformVersion();
+  }
+}
